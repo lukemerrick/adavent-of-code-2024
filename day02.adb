ee2cbace461838fd0327f2b319986a25f4481045
Luke Merrick <contact@lukemerrick.com>
2024-12-04 01:18:51 -0800

    Slight cleanup

--- day02.adb
+++ day02.adb
@@ -9,17 +9,15 @@ procedure Day02 is
      Ada.Containers.Vectors (Index_Type => Natural, Element_Type => Natural);
    use Natural_Vectors;
 
-   -- Go overboard with types.
    subtype Line_Num is Integer range 1 .. 1_000;
    type Increasing_Status is
      (Status_Unknown, Status_Increasing, Status_Decreasing);
 
    -- Procedure to read the next line into a preallocated vector.
    procedure Read_Report (Report_Line : String; Report : out Vector) is
-      Start_Index : Natural := 1;
+      Start_Index : Natural := Report_Line'First;
       Sep         : constant Character := ' ';
    begin
-      -- Empty the vector.
       Clear (Report);
 
       -- Go index-by-index, extracting on spaces.
@@ -58,7 +56,7 @@ procedure Day02 is
    end Is_Unsafe_Difference;
 
    -- Function for detecting unsafe patterns.
-   -- NOTE: This got really hacky to handle skipping. TODO: Write better Ada code!
+   -- NOTE: This got really hacky to handle skipping. TODO: Write a cleaner version!
    function Check_Report_Safety
      (Report : Vector; Skip : Integer := -1) return Integer
    is
@@ -77,6 +75,7 @@ procedure Day02 is
             Previous :=
               (if Skip = I - 1 then Report (I - 2) else Report (I - 1));
             Difference := Current - Previous;
+
             -- If we're just starting, set increasing status.
             if Increasing = Status_Unknown then
                Increasing :=
@@ -113,20 +112,21 @@ procedure Day02 is
    Safe_Count           : Natural := 0;
    Safe_Count_Dampened  : Natural := 0;
    File_Name            : constant String := "input/day02.txt";
-
 begin
    Start_Time := Clock;
    Open (F, In_File, File_Name);
    for I in Line_Num loop
-      Read_Report (Get_Line (F), Report);
-      --  Print_Report (Report);
-      --  Put_Line (Boolean'Image (Check_Report_Safety (Report)));
-      if Check_Report_Safety (Report) = -1 then
-         Safe_Count := Safe_Count + 1;
-      end if;
-      if Check_Report_Safety_Dampened (Report) then
-         Safe_Count_Dampened := Safe_Count_Dampened + 1;
-      end if;
+      declare
+         Report_Line : String := Get_Line (F);
+      begin
+         Read_Report (Report_Line, Report);
+         if Check_Report_Safety (Report) = -1 then
+            Safe_Count := Safe_Count + 1;
+         end if;
+         if Check_Report_Safety_Dampened (Report) then
+            Safe_Count_Dampened := Safe_Count_Dampened + 1;
+         end if;
+      end;
    end loop;
    Close (F);
    End_Time := Clock;
@@ -136,5 +136,4 @@ begin
      ("Procedure took"
       & Duration'Image (To_Duration (End_Time - Start_Time))
       & " seconds");
-
 end Day02;
